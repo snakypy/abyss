665b829533425f7d70bc49535b63f365a227b80a
William C. Canin <william.costa.canin@gmail.com>
2021-06-12 15:06:21 -0300

    :hammer: Calling user permission denial function and checking required tools.

--- snakypy/abyss/cli.py
+++ snakypy/abyss/cli.py
@@ -1,11 +1,18 @@
 """CLI - Command Line Interface"""
+from snakypy.abyss.utils.functools import deny_user_permission
+
 from snakypy.abyss.config import Config
 from snakypy.abyss.toolkit.encfs import Encfs
 from snakypy.abyss.utils.base import Base
 from snakypy.abyss.utils.menu import Menu
+from snakypy.helpers.catches import tools_requirements
 
 
 def main():
+    # Deny permission user by UID
+    deny_user_permission(uid=0)
+    # Tools requirements
+    tools_requirements("shred", "encfs")
     # Set configuration
     Config().set(Menu(), Base().config_file)
     # Encfs run
